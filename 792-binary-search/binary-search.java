abde03a8999141b6071b3e301fbc2d5baf3b7700
Basim Siddiqui <108823665+Basim03@users.noreply.github.com>
2023-08-24 12:33:43 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (94.88%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int s=0;
+        int end=nums.length-1;
+        while(s<=end)
+        {
+             int mid=(s+end)/2;
+             if(target==nums[mid]){
+                 return mid;
+             }
+             else if(nums[mid]>target)
+             end=mid-1;
+             else s=mid+1;
+
+        }
+        return -1;
+    }
+}
