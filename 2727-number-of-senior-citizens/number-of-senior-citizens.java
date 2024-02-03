6759e5bf7ef548a49be8532b25c2b7e4d6c9e847
Basim Siddiqui <108823665+Basim03@users.noreply.github.com>
2024-02-03 10:55:39 +0530

    Time: 1 ms (82.15%) | Memory: 42.5 MB (44.58%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -1,21 +1,10 @@
 class Solution {
     public int countSeniors(String[] details) {
-        int count=0;
-        for(int i=0;i<details.length;i++)
-        {
-            String s=details[i];
-            char third=s.charAt(s.length()-3);
-            char fourth=s.charAt(s.length()-4);
-            String f;
-            f=String.valueOf(fourth)+String.valueOf(third);
-            f=f.trim();
-                    int age = Integer.parseInt(f);
-                    System.out.println(age);
-
-            
-            if(age>60)
-            count++;
-        }
-        return count;
+      int count = 0;
+      for(String s : details) {
+          int age = Integer.parseInt(s.substring(11, 13));
+          if(age > 60) count ++;
+      }  
+      return count;
     }
 }
