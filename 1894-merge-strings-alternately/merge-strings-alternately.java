7cdaeb72f1734f9ee2b267f52413dadf9fb3c8ed
Basim Siddiqui <108823665+Basim03@users.noreply.github.com>
2024-03-25 19:55:23 +0530

    Time: 9 ms (8.60%) | Memory: 42.5 MB (21.69%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.java
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -1,27 +1,29 @@
 class Solution {
-  public String mergeAlternately(String word1, String word2) {
-    String ans = "";
-    int i = 0;
-    int j = 0;
+    public String mergeAlternately(String word1, String word2) {
+        String ans="";
+        int i=0;
+        int j=0;
+        
+        while(i<word1.length() && i<word2.length())
+        {
+        ans=ans+word1.charAt(i);
+       
+        ans+=word2.charAt(i);
+        i++;
+        }
+        if(i<word1.length())
+        {
+            for(int a=i;a<word1.length();a++)
+                   ans=ans+word1.charAt(a);
+        }
+        if(i<word2.length())
+         {
+            for(int a=i;a<word2.length();a++)
+                   ans=ans+word2.charAt(a);
+        }
 
-    while (i < word1.length() && i < word2.length()) {
-      ans += word1.charAt(i);
-      ans += word2.charAt(i);
-      i++;
+        System.out.println(ans);
+        return ans;
+
     }
-
-    if (i < word1.length()) {
-      for (; i < word1.length(); i++) {
-        ans += word1.charAt(i);
-      }
-    }
-
-    if (i < word2.length()) {
-      for (; i < word2.length(); i++) {
-        ans += word2.charAt(i);
-      }
-    }
-
-    return ans;
-  }
 }
