55cf891b95e8711e9667184d2b06f3ea6fee37b6
Basim Siddiqui <108823665+Basim03@users.noreply.github.com>
2024-04-07 13:48:27 +0530

    Time: 8 ms (5.83%) | Memory: 44.9 MB (97.13%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                int prev = (i == 0) ? 0 : flowerbed[i - 1];
+                int next = (i == flowerbed.length - 1) ? 0 : flowerbed[i + 1];
+                if (prev == 0 && next == 0) {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(flowerbed));
+        return n <= 0;
+    }
+}
