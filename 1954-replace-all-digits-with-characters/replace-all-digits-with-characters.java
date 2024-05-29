38e6874696465d6f8bc517bc3a096513f76b0b56
Basim Siddiqui <108823665+Basim03@users.noreply.github.com>
2024-05-29 22:47:58 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (72.08%) - LeetSync

--- 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.java
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.java
@@ -4,13 +4,13 @@ class Solution {
         char[] ans = s.toCharArray();
         for(int i=1;i<s.length();i+=2)
         {
-          char c=(char)(s.charAt(i-1)+s.charAt(i)-'0');
+          char c=Shift(s.charAt(i-1),s.charAt(i));
          ans[i]=c;
         }
         return new String(ans);
     }
     public static char Shift(char c,int n)
     {
-        return (char)(c+n);
+        return (char)(c+n-'0');
     }
 }
