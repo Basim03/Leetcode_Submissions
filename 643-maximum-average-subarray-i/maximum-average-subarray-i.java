33406cdd097674244a66f8b6df80e878c7f8ae3b
Basim Siddiqui <108823665+Basim03@users.noreply.github.com>
2024-06-07 09:42:59 +0530

    Time: 2637 ms (5.01%) | Memory: 56.4 MB (14.90%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+       int n = nums.length;
+        double maxAverage = Double.NEGATIVE_INFINITY;
+
+        // Iterate over each possible starting index of subarrays of length k
+        for (int i = 0; i <= n - k; i++) {
+            int currentSum = 0;
+
+            // Calculate the sum of the subarray starting at index i
+            for (int j = i; j < i + k; j++) {
+                currentSum += nums[j];
+            }
+
+            // Calculate the average and update maxAverage if it's the highest we've seen
+            double currentAverage = (double) currentSum / k;
+            if (currentAverage > maxAverage) {
+                maxAverage = currentAverage;
+            }
+        }
+
+        return maxAverage;
+    }
+}
