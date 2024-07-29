7858c35580ea1d14ab4a98531b730ed1c84a7ec4
Basim Siddiqui <108823665+Basim03@users.noreply.github.com>
2024-07-29 10:51:43 +0530

    Time: 1 ms (100.00%) | Memory: 43.2 MB (47.06%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
@@ -1,20 +1,22 @@
 class Solution {
     public int findLucky(int[] arr) {
-        int ans=-1;
-
-    
-        for(int i=0;i<arr.length;i++)
-        {
-                int count=0;
-            int current=arr[i];
-            for(int j=0;j<arr.length;j++)
-            {
-if(current==arr[j])
-count++;
-            }
-            if(current==count)
-            ans=Math.max(current,ans);
+        // Since the problem constraints specify 1 <= arr[i] <= 500,
+        // we can use an array of size 501 to count occurrences.
+        int[] count = new int[501];
+        
+        // Count occurrences of each number
+        for (int num : arr) {
+            count[num]++;
         }
+        
+        int ans = -1;
+        // Find the largest lucky number
+        for (int i = 1; i < count.length; i++) {
+            if (count[i] == i) {
+                ans = i;
+            }
+        }
+        
         return ans;
     }
 }
