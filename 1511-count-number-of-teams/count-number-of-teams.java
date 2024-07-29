9e834583a3d011897e9105b9f1770bc2eb626806
Basim Siddiqui <108823665+Basim03@users.noreply.github.com>
2024-07-29 10:38:33 +0530

    Time: 16 ms (82.28%) | Memory: 42.4 MB (45.15%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.java
+++ 1511-count-number-of-teams/count-number-of-teams.java
@@ -1,15 +1,31 @@
 class Solution {
     public int numTeams(int[] rating) {
+        int n = rating.length;
         int count = 0;
-        for (int i = 0; i < rating.length - 2; i++) {
-            for (int j = i + 1; j < rating.length - 1; j++) {
-                for (int k = j + 1; k < rating.length; k++) {
-                    if ((rating[i] < rating[j] && rating[j] < rating[k]) || (rating[i] > rating[j] && rating[j] > rating[k])) {
-                        count++;
-                    }
+        
+        for (int j = 0; j < n; j++) {
+            int leftSmaller = 0, rightGreater = 0;
+            int leftGreater = 0, rightSmaller = 0;
+            
+            for (int i = 0; i < j; i++) {
+                if (rating[i] < rating[j]) {
+                    leftSmaller++;
+                } else if (rating[i] > rating[j]) {
+                    leftGreater++;
                 }
             }
+            
+            for (int k = j + 1; k < n; k++) {
+                if (rating[k] > rating[j]) {
+                    rightGreater++;
+                } else if (rating[k] < rating[j]) {
+                    rightSmaller++;
+                }
+            }
+            
+            count += leftSmaller * rightGreater + leftGreater * rightSmaller;
         }
+        
         return count;
     }
 }
