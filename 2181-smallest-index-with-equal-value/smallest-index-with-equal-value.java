11729c3dd8209d01b4718d65901882fe04cbecdd
Basim Siddiqui <108823665+Basim03@users.noreply.github.com>
2024-08-05 10:24:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (25.09%) - LeetSync

--- 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.java
+++ 2181-smallest-index-with-equal-value/smallest-index-with-equal-value.java
@@ -1,17 +1,10 @@
 class Solution {
     public int smallestEqual(int[] nums) {
-        int ans=Integer.MAX_VALUE;
-        int current=0;
-        // int arr[]=new int[nums.length];
-        for(int i=0;i<nums.length;i++)
-        {
-           current=i%10;
-           if(current==nums[i])
-           {
-            ans=Math.min(ans,i);
-           }
+        for(int i=0; i<nums.length; i++) {
+            if(i % 10 == nums[i]) {
+                return i;
+            }
         }
-        return ans == Integer.MAX_VALUE ? -1 : ans;
-        
+        return -1;
     }
 }
