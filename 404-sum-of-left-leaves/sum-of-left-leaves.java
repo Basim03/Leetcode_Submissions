c420fc18d6c26a7b78566c64c390c385a8416f14
Basim Siddiqui <108823665+Basim03@users.noreply.github.com>
2024-09-30 15:27:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (5.50%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.java
+++ 404-sum-of-left-leaves/sum-of-left-leaves.java
@@ -1,46 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+        int sum=0;
     public int sumOfLeftLeaves(TreeNode root) {
-        if(root==null)
-        {
-            return 0;
-        }
-        int ans=0;
-        Queue<TreeNode> queue=new LinkedList<>();
-queue.offer(root);
- while(!queue.isEmpty())
- {
-    int levelSize=queue.size();
-for(int i=0;i<levelSize;i++)
-{
-TreeNode currentNode=queue.poll();
-if(currentNode.left!=null)
-{
-    queue.offer(currentNode.left);
-    if(currentNode.left.left==null && currentNode.left.right==null)
-    ans+=currentNode.left.val;
-}
-if(currentNode.right!=null)
-{
-    queue.offer(currentNode.right);
-}
-}
+  if(root==null)
+  {
+    return 0;
 
- }
- return ans;
+  }
+  helper(root,false);
+  return sum;
+    }
+    public void helper(TreeNode root,boolean isLeft)
+    {
+        if(root==null)
+        return ;
+        else 
+        {
+            if(root.left==null && root.right==null && isLeft)
+            {
+                sum+=root.val;
+            }
+            helper(root.left,true);
+            helper(root.right,false);
+        }
     }
 }
