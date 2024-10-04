9248629fdda562b03d30ee783d56276abe1b4711
Basim Siddiqui <108823665+Basim03@users.noreply.github.com>
2024-10-04 11:57:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (83.15%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -15,16 +15,14 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-      return isMirror(root.left,root.right);
-
+     return helper(root.left,root.right);
     }
-    private boolean isMirror(TreeNode node1, TreeNode node2) {
-        if (node1 == null && node2 == null) {
-            return true;
-        }
-        if (node1 == null || node2 == null) {
-            return false;
-        }
-        return node1.val == node2.val && isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
+    public boolean helper(TreeNode L,TreeNode R)
+    {
+        if(L==null && R==null)
+        return true;
+        else if(L==null || R==null)
+        return false;
+        return L.val==R.val && helper(L.left,R.right) && helper(L.right,R.left);
     }
 }
